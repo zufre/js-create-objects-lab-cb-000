e4573f601abbaca6cd4597dc16bbb5bc6991690f
Scott C. Reynolds <scott@scottcreynolds.com>
2016-07-01 17:04:17 -0400

    update tests, move location, dump requires.yml

--- test/object-spec.js
+++ test/object-spec.js
@@ -11,7 +11,7 @@ describe('objects', () => {
   describe('Dog constructor', function(){
     it("should create a new dog with three properties", function(){
       var caldwell = new Dog("caldwell", "toy poodle", "4 months");
-      expect(caldwell.constructor == Dog).toEqual(true);
+      expect(caldwell).toBeA(Dog);
       expect(caldwell.name).toEqual("caldwell");
       expect(caldwell.age).toEqual("4 months");
       expect(caldwell.breed).toEqual("toy poodle");
@@ -22,7 +22,7 @@ describe('objects', () => {
   describe('Cat constructor', function(){
     it("should create a new cat with three properties", function(){
       var puff = new Cat("Professor Puff", "Maine Coon", "6 years");
-      expect(puff.constructor == Cat).toEqual(true);
+      expect(puff).toBeA(Cat);
       expect(puff.name).toEqual("Professor Puff");
       expect(puff.age).toEqual("6 years");
       expect(puff.breed).toEqual("Maine Coon");
@@ -33,7 +33,7 @@ describe('objects', () => {
   describe('Ferret constructor', function(){
     it("should create a new ferret with three properties", function(){
       var sugarDaddy = new Ferret("Sugar Daddy", "2 years", "raw meat");
-      expect(sugarDaddy.constructor == Ferret).toEqual(true);
+      expect(sugarDaddy).toBeA(Ferret);
       expect(sugarDaddy.name).toEqual("Sugar Daddy");
       expect(sugarDaddy.age).toEqual("2 years");
       expect(sugarDaddy.foodRequirements).toEqual("raw meat");
@@ -44,7 +44,7 @@ describe('objects', () => {
   describe('Bird constructor', function(){
     it("should create a new bird with two properties", function(){
       var darla = new Bird("Darla", "cockatoo");
-      expect(darla.constructor == Bird).toEqual(true);
+      expect(darla).toBeA(Bird);
       expect(darla.name).toEqual("Darla");
       expect(darla.breed).toEqual("cockatoo");
     });
@@ -54,7 +54,7 @@ describe('objects', () => {
   describe('Fish constructor', function(){
     it("should create a new fish with three properties", function(){
       var pinocchio = new Fish("Pinocchio", "parrot fish", "salt water");
-      expect(pinocchio.constructor == Fish).toEqual(true);
+      expect(pinocchio).toBeA(Fish);
       expect(pinocchio.name).toEqual("Pinocchio");
       expect(pinocchio.species).toEqual("parrot fish");
       expect(pinocchio.waterConditions).toEqual("salt water");
@@ -62,5 +62,4 @@ describe('objects', () => {
 
   });
 });
-
 
